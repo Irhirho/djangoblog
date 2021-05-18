bc6cbf1a8b1bb6a9aa8bedfb4090d9df1182e7cf
Irhirho <tq23084zay027542@126.com>
2021-05-18 10:02:58 +0800

    the blog is finished ,more django content to learn, gogogo

---
+++ hello/blog/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+from models import Article
+
+info_dict = {
+'queryset': Article.objects.all(),
+}
+
+#urlpatterns = patterns('',
+#(r'^$', 'django.views.generic.list_detail.object_list', info_dict),
+#(r'^(?Pd+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
+#)
+
+
+urlpatterns = patterns('',
+		    
+	(r'^$', 'django.views.generic.list_detail.object_list', info_dict),
+	    
+	(r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
+	)
